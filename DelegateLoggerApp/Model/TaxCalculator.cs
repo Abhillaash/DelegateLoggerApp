e24fd40197bc81612f50d52fabb90ff861b33605
Abhilash <abhilashjain1919@gmail.com>
2023-08-16 12:34:41 +0530

    first commit

---
+++ DelegateLoggerApp/Model/TaxCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateLoggerApp.Model
+{
+    delegate void DLoggerDelegate(string err);
+    class TaxCalculator
+    {
+        
+         
+        public TaxCalculator(int amount, int rate,DLoggerDelegate ob)
+        {
+            Amount = amount;
+            Rate = rate;
+            dLoggerDelegate = ob;
+
+            
+        }
+
+        public int Amount { get; set; }
+        public int Rate { get; set; }
+
+        DLoggerDelegate dLoggerDelegate;
+        //DLoggerDelegate ob = DBLogger;
+        //ob += XMLLogger;
+        
+
+        
+        //public DBLogger DBLogger { get; set; }
+
+        public int CalculateTax()
+        {
+            int tax = 0;
+            try
+            {
+
+                tax = Amount * Rate / 0;
+            }
+            catch (Exception ex)
+            {
+
+                //DBLogger.Log("Divided by zero");
+                dLoggerDelegate("Divided by zero");
+
+
+            }
+            return tax;
+        }
+        public static void DBLogger(string err)
+        {
+            Console.WriteLine("Logged to  Database : " + err);
+        }
+        public static void XMLLogger(string err)
+        {
+            Console.WriteLine("Logged to Database : " + err);
+        }
+    }
+}
---
+++ DelegateLoggerApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DelegateLoggerApp.Model;
+
+namespace DelegateLoggerApp
+{
+    
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaxCalculator taxCalculator = new TaxCalculator(10000, 2,TaxCalculator.DBLogger);
+            Console.WriteLine(taxCalculator.CalculateTax());
+        }
+    }
+}
